e6ccbf0cfadaf1ec6b0810fe39d96b4af289e489
lmb <i@lmb.io>
2019-05-05 10:13:06 -0400

    Build plain kernels for use with virtme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ci-kernels
-A collection of kernels used for CI builds
+A collection of kernels used for CI builds. You'll need the tools to build the Linux kernel
+and [virtme](https://github.com/amluto/virtme).
--- make.sh
+++ make.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-set -eux
+set -eu
 set -o pipefail
 
 readonly script_dir="$(cd $(dirname "$0"); pwd)"
-readonly rootfs_dir="${script_dir}/rootfs"
 readonly build_dir="${script_dir}/build"
-
-pushd "${rootfs_dir}"
-find . -print0 | cpio --null -o --quiet --format=newc | gzip -9 > "${script_dir}/initramfs.cpio.gz"
-zcat "${script_dir}/initramfs.cpio.gz" | cpio -v --list
-popd
 
 mkdir -p "${build_dir}"
 
-readonly kernel_versions=("4.19.14")
+readonly kernel_versions=("4.19.40" "5.0.13")
 for kernel_version in "${kernel_versions[@]}"; do
+	if [[ -f "linux-${kernel_version}.bz" ]]; then
+		echo Skipping ${kernel_version}, it already exist
+		continue
+	fi
+
 	readonly src_dir="${build_dir}/linux-${kernel_version}"
 	readonly archive="${build_dir}/linux-${kernel_version}.tar.xz"
 
-	test -e "${archive}" || curl -L https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${kernel_version}.tar.xz -o "${archive}"
+	test -e "${archive}" || curl --fail -L https://cdn.kernel.org/pub/linux/kernel/v${kernel_version%%.*}.x/linux-${kernel_version}.tar.xz -o "${archive}"
 	test -d "${src_dir}" || tar --xz -xf "${archive}" -C "${build_dir}"
 
-	make -C "${src_dir}" KCONFIG_CONFIG=custom.config defconfig
+	pushd "${src_dir}"
+	make KCONFIG_CONFIG=custom.config defconfig
 	cat "${script_dir}/config" >> "${src_dir}/custom.config"
-	make -C "${src_dir}" allnoconfig KCONFIG_ALLCONFIG="custom.config"
+	make allnoconfig KCONFIG_ALLCONFIG="custom.config"
+	virtme-configkernel --update
 
-	make -C "${src_dir}" clean
-	make -C "${src_dir}" -j$(nproc) bzImage
+	make clean
+	make -j$(nproc) bzImage
 
-	mv "${src_dir}/arch/x86/boot/bzImage" "${script_dir}/linux-${kernel_version}"
+	mv "arch/x86/boot/bzImage" "${script_dir}/linux-${kernel_version}.bz"
+	popd
 done
 
 
