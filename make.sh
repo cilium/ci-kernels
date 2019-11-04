41b651bbd56867859281172f1659597a49097fcb
Lorenz Bauer <lmb@cloudflare.com>
2019-11-04 15:31:18 +0000

    Update kernels

--- make.sh
+++ make.sh
@@ -3,20 +3,20 @@
 set -eu
 set -o pipefail
 
-readonly script_dir="$(cd $(dirname "$0"); pwd)"
+readonly script_dir="$(cd "$(dirname "$0")"; pwd)"
 readonly build_dir="${script_dir}/build"
 
 mkdir -p "${build_dir}"
 
-readonly kernel_versions=("4.19.40" "5.0.13")
+readonly kernel_versions=("4.9.198" "4.19.81" "5.3.8")
 for kernel_version in "${kernel_versions[@]}"; do
 	if [[ -f "linux-${kernel_version}.bz" ]]; then
 		echo Skipping ${kernel_version}, it already exist
 		continue
 	fi
 
-	readonly src_dir="${build_dir}/linux-${kernel_version}"
-	readonly archive="${build_dir}/linux-${kernel_version}.tar.xz"
+	src_dir="${build_dir}/linux-${kernel_version}"
+	archive="${build_dir}/linux-${kernel_version}.tar.xz"
 
 	test -e "${archive}" || curl --fail -L https://cdn.kernel.org/pub/linux/kernel/v${kernel_version%%.*}.x/linux-${kernel_version}.tar.xz -o "${archive}"
 	test -d "${src_dir}" || tar --xz -xf "${archive}" -C "${build_dir}"
@@ -28,7 +28,7 @@ for kernel_version in "${kernel_versions[@]}"; do
 	virtme-configkernel --update
 
 	make clean
-	make -j$(nproc) bzImage
+	make -j7 bzImage
 
 	mv "arch/x86/boot/bzImage" "${script_dir}/linux-${kernel_version}.bz"
 	popd
