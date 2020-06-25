64028bbc6dea7e7ee24b70a7040af5e4da589af6
Lorenz Bauer <lmb@cloudflare.com>
2020-06-25 15:14:07 +0100

    Package bpf selftests

--- make.sh
+++ make.sh
@@ -3,6 +3,7 @@
 set -eu
 set -o pipefail
 
+readonly clang="${CLANG:-clang-10}"
 readonly script_dir="$(cd "$(dirname "$0")"; pwd)"
 readonly build_dir="${script_dir}/build"
 
@@ -11,15 +12,15 @@ mkdir -p "${build_dir}"
 readonly kernel_versions=("4.9.226" "4.14.183" "4.19.127" "5.4.45" "5.7.1")
 for kernel_version in "${kernel_versions[@]}"; do
 	if [[ -f "linux-${kernel_version}.bz" ]]; then
-		echo Skipping ${kernel_version}, it already exist
+		echo "Skipping ${kernel_version}, it already exist"
 		continue
 	fi
 
 	src_dir="${build_dir}/linux-${kernel_version}"
 	archive="${build_dir}/linux-${kernel_version}.tar.xz"
-	major_version="$(echo "$kernel_version" | cut -d . -f 1-2)"
+	series="$(echo "$kernel_version" | cut -d . -f 1-2)"
 
-	test -e "${archive}" || curl --fail -L https://cdn.kernel.org/pub/linux/kernel/v${kernel_version%%.*}.x/linux-${kernel_version}.tar.xz -o "${archive}"
+	test -e "${archive}" || curl --fail -L "https://cdn.kernel.org/pub/linux/kernel/v${kernel_version%%.*}.x/linux-${kernel_version}.tar.xz" -o "${archive}"
 	test -d "${src_dir}" || tar --xz -xf "${archive}" -C "${build_dir}"
 
 	pushd "${src_dir}"
@@ -31,10 +32,31 @@ for kernel_version in "${kernel_versions[@]}"; do
 	make clean
 	make -j7 bzImage
 
-	mv "arch/x86/boot/bzImage" "${script_dir}/linux-${kernel_version}.bz"
-	popd
+	cp "arch/x86/boot/bzImage" "${script_dir}/linux-${kernel_version}.bz"
+	cp -f "${script_dir}/linux-${kernel_version}.bz" "${script_dir}/linux-${series}.bz"
 
-	cp -f "${script_dir}/linux-${kernel_version}.bz" "${script_dir}/linux-${major_version}.bz"
+	if [ -d "tools/testing/selftests/bpf" ]; then
+		if [ "${series}" = "4.14" ]; then
+			inc="$(find /usr/include -iregex '.+/asm/bitsperlong\.h$' | head -n 1)"
+			export CLANG="$clang '-I${inc%asm/bitsperlong.h}'"
+		else
+			export CLANG="$clang"
+		fi
+
+		make -C tools/testing/selftests/bpf
+		while IFS= read -r obj; do
+			if readelf -h "$obj" | grep -q "Linux BPF"; then
+				if [ "${series}" = "4.19" ]; then
+					# Remove .BTF.ext, since .BTF is rewritten by pahole.
+					# See https://lore.kernel.org/bpf/CACAyw9-cinpz=U+8tjV-GMWuth71jrOYLQ05Q7_c34TCeMJxMg@mail.gmail.com/
+					llvm-objcopy --remove-section .BTF.ext "$obj" 1>&2
+				fi
+				echo "$obj"
+			fi
+		done < <(find tools/testing/selftests/bpf -type f -name "*.o") | tar cvjf "${script_dir}/linux-${kernel_version}-selftests-bpf.bz" -T -
+		cp -f "${script_dir}/linux-${kernel_version}-selftests-bpf.bz" "${script_dir}/linux-${series}-selftests-bpf.bz"
+	fi
+	popd
 done
 
 
