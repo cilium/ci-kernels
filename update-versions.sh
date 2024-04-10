7a16d9e6defcd08b39eea6351fd1a2ed93d841bc
Tobias Klauser <tobias@cilium.io>
2024-04-10 16:15:14 +0200

    Update kernel versions
    Add 6.8 stable and 6.9-rc. This will allow to use 6.8 in cilium/ebpf CI
    instead of 6.7 which is EOL.
    
    Also update update-version.sh to filter out EOL releases. Turns out this
    is easier to achieve using https://www.kernel.org/releases.json as a
    source and using just jq for JSON transformation.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- update-versions.sh
+++ update-versions.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # Fetch the releases encoded in the kernel.org homepage and store them as a JSON.
 
-exec curl -sL https://www.kernel.org/ |
-xmllint --html --xpath "//table[@id='releases']/tr[.//a[contains(text(), 'tarball')]]/td/strong/text() | //table[@id='releases']/tr[.//a[contains(text(), 'tarball')]]/td[1]/text()" - 2>/dev/null |
-awk '
-  BEGIN { ORS=""; print "[" }
-  /:/ { gsub(/:/, ""); type=$0; getline; version=$0; print (NR>2?",":"") "{\"version\": \"" version "\", \"type\": \"" type "\"}" }
-  END { print "]" }
-' |
-jq '.' | tee versions.json
+exec curl -sL https://www.kernel.org/releases.json |
+	jq '[ .releases[] |
+		select(.iseol == false and .moniker != "linux-next") |
+		{ "version" : .version, "type" : .moniker }]' |
+	tee versions.json
