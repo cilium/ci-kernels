502d34578d1fe4aa8ff7f223de981621ea16ce81
Lorenz Bauer <lmb@isovalent.com>
2024-05-23 16:27:40 +0100

    only tag selftests for static tags
    We can currently get into a situation where the selftests tag is
    out of sync with the non-selftest one. For example:
    
        6.8           # on 6.8.X+1
        6.8-selftests # on 6.8.X
    
    This is because we only build selftests for latest and mainline
    kernel. Avoid this by only tagging selftests when a static tag
    is used.
    
    Also avoid using the same static_tag multiple times due to duplicate
    "stable" entries in releases.json.
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- update-versions.sh
+++ update-versions.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # Fetch the releases encoded in the kernel.org homepage and store them as a JSON.
 
-exec curl -sL https://www.kernel.org/releases.json |
-	jq '[ .releases[] |
-		select(.iseol == false and .moniker != "linux-next") |
-		{ "version" : .version, "type" : .moniker }]' |
-	tee versions.json
+read -r -d '' filter <<'EOF'
+[ .releases[]
+	| select(.iseol == false and .moniker != "linux-next")
+	| { "version" : .version, "type" : .moniker, "static_tag": "" }
+]
+	| group_by(.type)
+	| map(.[0].static_tag = .[0].type)
+	| flatten
+	| map({version, static_tag})
+	| sort_by(.version) | reverse
+EOF
+
+exec curl -sL https://www.kernel.org/releases.json | jq "$filter" | tee versions.json
+
