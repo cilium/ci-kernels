b99221bdbaac576063553782a4ce93ced2ca55b3
Lorenz Bauer <lmb@isovalent.com>
2025-03-20 14:38:03 +0000

    Package debug scripts
    See https://www.kernel.org/doc/html/latest/process/debugging/gdb-kernel-debugging.html
    
    Signed-off-by: Lorenz Bauer <lmb@isovalent.com>

--- copy-debug.sh
+++ copy-debug.sh
@@ -9,6 +9,12 @@ readonly output="${1}"
 # Retain vmlinux which includes debug symbols
 cp vmlinux "$output/boot/"
 
+# Retain debug scripts
+mkdir -p "$output/usr/src/linux"
+cp -v vmlinux-gdb.py "$output/usr/src/linux"
+ln -s ../usr/src/linux/vmlinux-gdb.py "$output/boot/vmlinux-gdb.py"
+find . -path "*/scripts/gdb*" -type f -not -path "*/__pycache__*" -exec cp -v --parents {} "$output/usr/src/linux" \;
+
 # Retain only the sources referenced by dwarf debug info
 "${LLVM_DWARFDUMP}" "$output/boot/vmlinux" | \
 	awk -f filter-debug.awk | \
