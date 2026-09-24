0d3b88fdf5e229558d86d5f252958d10d7e663a0
Timo Beckers <timo@isovalent.com>
2026-09-24 11:26:12 +0200

    build: remove 4.x compatibility hacks
    Signed-off-by: Timo Beckers <timo@isovalent.com>

--- build-selftests.sh
+++ build-selftests.sh
@@ -11,18 +11,5 @@ source "$dir/env.sh"
 # Can't figure out how to use cross compilation with selftests.
 unset CROSS_COMPILE
 
-series="$(echo "${KERNEL_VERSION}" | cut -d . -f 1-2)"
-readonly series
-
-if [[ "${series}" = "4.9" ]]; then
-	echo "No selftests on <= 4.9"
-	exit 0
-fi
-
-if [ "${series}" = "4.14" ]; then
-	inc="$(find /usr/include -iregex '.+/asm/bitsperlong\.h$' | head -n 1)"
-	export CLANG="$CLANG '-I${inc%asm/bitsperlong.h}'"
-fi
-
 make headers
 make -C tools/testing/selftests/bpf -j "$(nproc)"
